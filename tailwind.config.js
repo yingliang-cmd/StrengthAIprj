71d6438b9fcc81fa3d806899302d2ceb362e73b6
yingliang-cmd <skyfly317@gmail.com>
2025-11-10 16:56:24 +0000

    ver06

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        main: '#166B6C',
-      },
-      fontFamily: {
-        sans: ['Inter', 'Noto Sans TC', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
