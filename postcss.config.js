9f001bf6301929ecd1e4519f5815d0c755b86b83
yingliang-cmd <skyfly317@gmail.com>
2025-11-11 04:23:03 +0000

    ver09

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+// postcss.config.js
+export default {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
-};
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// 這裡一定要改成你的 repo 名稱，GitHub Pages 用：
 export default defineConfig({
   plugins: [react()],
   base: '/StrengthAIprj/',
